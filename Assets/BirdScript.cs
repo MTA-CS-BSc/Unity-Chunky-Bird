73a56d4e7f6bbdb8cb3546c6fc36408e0a1a8be3
mayaraskin <mayaraskin14@gmail.com>
2024-09-15 16:32:11 +0300

    Added game over on jump off boundaries

--- Assets/BirdScript.cs
+++ Assets/BirdScript.cs
@@ -9,6 +9,7 @@ public class BirdScript : MonoBehaviour
     public float jumpStrength;
     public GameplayScript gameplayScript;
     public bool isAlive = true;
+    private readonly float _yDeathPoint = 12;
     
     void Start() {
         gameplayScript = GameObject.FindGameObjectWithTag("Gameplay").GetComponent<GameplayScript>();
@@ -17,6 +18,9 @@ public class BirdScript : MonoBehaviour
     void Update() {
         if (isAlive && Input.GetKeyDown(KeyCode.Space))
             rigidbody.velocity = Vector2.up * jumpStrength;
+        
+        if (isAlive && (transform.position.y < -_yDeathPoint || transform.position.y > _yDeathPoint))
+            gameplayScript.GameOver();
     }
 
     private void OnCollisionEnter2D(Collision2D other) {
