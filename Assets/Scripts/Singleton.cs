98919d62308db1bc144ad8e13825020278c44a45
mayaraskin <mayaraskin14@gmail.com>
2024-09-19 22:54:26 +0300

    Removed redundant code

--- Assets/Scripts/Singleton.cs
+++ Assets/Scripts/Singleton.cs
@@ -5,7 +5,6 @@ public abstract class Singleton<T> : MonoBehaviour where T: MonoBehaviour
 {
     public static T Instance { get; private set; }
     
-    // Self implemented optimized code
     protected virtual void Awake() {
         if (Instance == null) {
             Instance = FindObjectOfType<T>();
