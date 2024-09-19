365ee3cb0db5696429dee6e756d056dc24ec548f
mayaraskin <mayaraskin14@gmail.com>
2024-09-19 21:35:35 +0300

    Changed script name

--- Assets/Scripts/Bird.cs
+++ Assets/Scripts/Bird.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BirdScript : MonoBehaviour
+public class Bird : MonoBehaviour
 {
     public Rigidbody2D rigidbody;
     public float jumpStrength;
--- Assets/Scripts/PipesMiddleScript.cs
+++ Assets/Scripts/PipesMiddleScript.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PipeMiddleScript : MonoBehaviour
+public class PipesMiddleScript : MonoBehaviour
 {
     private GameplayScript _gameplayScript;
     
